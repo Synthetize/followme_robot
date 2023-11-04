361517f2231a3ab84429af7c26129d6ee35e5973
Leonardo Salpini <leonardo.salpini00@gmail.com>
2023-11-04 15:30:43 +0100

    CartesianCoordinate removed setters and change type to record,

--- api/src/main/java/it/unicam/cs/followme/list/model/CartesianCoordinate.java
+++ api/src/main/java/it/unicam/cs/followme/list/model/CartesianCoordinate.java
@@ -1,33 +1,6 @@
 package it.unicam.cs.followme.list.model;
 
-import java.security.PublicKey;
 
-public class CartesianCoordinate implements Coordinate<CartesianCoordinate>{
-    private double x;
-    private double y;
+public record CartesianCoordinate(double xValue, double yValue) implements Coordinate {
 
-    public CartesianCoordinate(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return this.x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return this.y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-    @Override
-    public double calculateDistance(CartesianCoordinate coordinates) {
-        return Math.sqrt(Math.pow(coordinates.getX() - this.x, 2) + Math.pow(coordinates.getY() - this.y, 2));
-    }
 }
--- api/src/test/java/it/unicam/cs/followme/list/model/CartesianCoordinateTest.java
+++ api/src/test/java/it/unicam/cs/followme/list/model/CartesianCoordinateTest.java
@@ -1,5 +1,4 @@
 package it.unicam.cs.followme.list.model;
-import com.sun.tools.jconsole.JConsoleContext;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,40 +10,16 @@ public class CartesianCoordinateTest {
     public void createCartesianCoordinates() {
         assertDoesNotThrow(() -> new CartesianCoordinate(1.0, 1.0));
     }
+
     @Test
     public void testGetX() {
         CartesianCoordinate cartesianCoordinates = new CartesianCoordinate(1.0, 1.0);
-        assertEquals(1.0, cartesianCoordinates.getX());
-    }
-
-    @Test
-    public void shouldSetXValue() {
-        CartesianCoordinate cartesianCoordinates = new CartesianCoordinate(1.0, 1.0);
-        cartesianCoordinates.setX(2.0);
-        assertEquals(2.0, cartesianCoordinates.getX());
+        assertEquals(1.0, cartesianCoordinates.xValue());
     }
 
     @Test
     public void testGetY() {
         CartesianCoordinate cartesianCoordinates = new CartesianCoordinate(1.0, 1.0);
-        assertEquals(1, cartesianCoordinates.getY());
-    }
-
-    @Test
-    public void shouldSetYValue() {
-        CartesianCoordinate cartesianCoordinates = new CartesianCoordinate(1.0, 1.0);
-        cartesianCoordinates.setY(2.0);
-        assertEquals(2.0, cartesianCoordinates.getY());
-    }
-
-    @Test
-    public void testCalculateDistance() {
-        CartesianCoordinate cartesianCoordinates = new CartesianCoordinate(1.0, 1.0);
-        CartesianCoordinate cartesianCoordinates2 = new CartesianCoordinate(1.0, 1.0);
-        assertEquals(0.0, cartesianCoordinates.calculateDistance(cartesianCoordinates2));
-        CartesianCoordinate cartesianCoordinates3 = new CartesianCoordinate(7.0, 5.0);
-        assertEquals(7.211102550927978, cartesianCoordinates.calculateDistance(cartesianCoordinates3));
-        CartesianCoordinate cartesianCoordinates4 = new CartesianCoordinate(-4.0, 10);
-        assertEquals(10.29563014098700, cartesianCoordinates.calculateDistance(cartesianCoordinates4));
+        assertEquals(1, cartesianCoordinates.yValue());
     }
 }
