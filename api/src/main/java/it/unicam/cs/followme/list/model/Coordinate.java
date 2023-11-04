29ec7b0816af39318f3d65be4af4d1214246988f
Leonardo Salpini <leonardo.salpini00@gmail.com>
2023-11-04 15:33:06 +0100

    removed calculateDistance from coordinate, removed hasRobotInside from shape interface

--- api/src/main/java/it/unicam/cs/followme/list/model/Coordinate.java
+++ api/src/main/java/it/unicam/cs/followme/list/model/Coordinate.java
@@ -3,11 +3,5 @@ package it.unicam.cs.followme.list.model;
 /**
  * This interface is used to rappresent a generic coordinate
  */
-public interface Coordinate<T extends Coordinate<T>> {
-    /**
-     * This method is used to calculate the distance between two coordinates
-     * @param coordinates the coordinates to calculate the distance
-     * @return the distance between the two coordinates
-     */
-    double calculateDistance(T coordinates);
+public interface Coordinate {
 }
