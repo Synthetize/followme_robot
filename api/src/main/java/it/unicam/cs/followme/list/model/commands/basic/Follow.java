dc64f7e05e36c6c9f551791fe0cebf37453de8dc
Leonardo Salpini <leonardo.salpini00@gmail.com>
2023-11-12 18:02:01 +0100

    changes

--- api/src/main/java/it/unicam/cs/followme/list/model/commands/basic/Follow.java
+++ api/src/main/java/it/unicam/cs/followme/list/model/commands/basic/Follow.java
@@ -2,9 +2,7 @@ package it.unicam.cs.followme.list.model.commands.basic;
 
 import it.unicam.cs.followme.list.model.Environment;
 import it.unicam.cs.followme.list.model.commands.Command;
-import it.unicam.cs.followme.list.model.robots.BasicRobot;
 import it.unicam.cs.followme.list.model.robots.Robot;
-import it.unicam.cs.followme.list.model.shapes.Shape;
 import it.unicam.cs.followme.list.model.utils.CartesianCoordinate;
 import it.unicam.cs.followme.list.model.utils.Coordinate;
 import it.unicam.cs.followme.utilities.RobotCommand;
@@ -17,13 +15,13 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Follow<R extends Robot> implements Command<R> {
     private final Environment<R> environment;
     private final String signal;
-    private final int distance;
+    private final int distanceFromRobot;
     private final int speed;
 
     public Follow(String label, double[] args, Environment<R> environment) {
         this.environment = environment;
         this.signal = label;
-        this.distance = (int) args[0];
+        this.distanceFromRobot = (int) args[0];
         this.speed = (int) args[1];
     }
 
@@ -34,33 +32,34 @@ public class Follow<R extends Robot> implements Command<R> {
 
     @Override
     public void run(R robot) {
-        Map<R, Coordinate> robotList = environment.getRobotsDetails();
-        HashMap<R, Coordinate> robotWithLabelBetweenDistance = new HashMap<>();
-        robotList.forEach((robotElement, coordinate) -> {
-            if (robotElement.getCurrentConditionLabels().contains(signal)) {
-                double distance = environment.getDistanceBetweenTwoCoordinates(coordinate, environment.getRobotCoordinate(robot));
-                if (distance <= this.distance) {
-                    if (!(robotElement.equals(robot))) {
-                        robotWithLabelBetweenDistance.put(robotElement, coordinate);
-                    }
-                }
-            }
-        });
+        HashMap<R, Coordinate> robotList = getRobotsWithLabelBetweenDistance(robot);
         final AtomicReference<Double> xAvgValue = new AtomicReference<>((double) 0);
         final AtomicReference<Double> yAvgValue = new AtomicReference<>((double) 0);
-        if (robotWithLabelBetweenDistance.isEmpty()) {
+        if (robotList.isEmpty()) {
             Random random = new Random();
-            xAvgValue.updateAndGet(v -> random.nextDouble(2 * this.distance) - this.distance);
-            yAvgValue.updateAndGet(v -> random.nextDouble(2 * this.distance) - this.distance);
+            xAvgValue.updateAndGet(v -> random.nextDouble(2 * this.distanceFromRobot) - this.distanceFromRobot);
+            yAvgValue.updateAndGet(v -> random.nextDouble(2 * this.distanceFromRobot) - this.distanceFromRobot);
         } else {
-            robotWithLabelBetweenDistance.forEach((shape, coordinate) -> {
+            robotList.forEach((shape, coordinate) -> {
                 xAvgValue.updateAndGet(v -> v + coordinate.getX());
                 yAvgValue.updateAndGet(v -> v + coordinate.getY());
             });
-            xAvgValue.updateAndGet(v -> v / robotWithLabelBetweenDistance.size());
-            yAvgValue.updateAndGet(v -> v / robotWithLabelBetweenDistance.size());
+            xAvgValue.updateAndGet(v -> v / robotList.size());
+            yAvgValue.updateAndGet(v -> v / robotList.size());
         }
         Move<R> move = new Move<>(new CartesianCoordinate(xAvgValue.get(), yAvgValue.get()), speed, environment);
         move.run(robot);
+    }
+
+    private HashMap<R, Coordinate> getRobotsWithLabelBetweenDistance(R robot) {
+        Map<R, Coordinate> robotList = environment.getRobotsDetails();
+        HashMap<R, Coordinate> robotWithLabelBetweenDistance = new HashMap<>();
+        robotList.forEach((robotElement, coordinate) -> {
+            if(robotElement.equals(robot)) return;
+            if(!(robotElement.getCurrentConditionLabels().contains(signal))) return;
+            if(environment.getDistanceBetweenTwoCoordinates(coordinate, environment.getRobotCoordinate(robot)) > this.distanceFromRobot) return;
+            robotWithLabelBetweenDistance.put(robotElement, coordinate);
+        });
+        return robotWithLabelBetweenDistance;
     }
 }
