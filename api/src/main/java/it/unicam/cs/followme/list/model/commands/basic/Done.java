f1066a7343a87f2dc0a1a242bf25b518076200da
Leonardo Salpini <leonardo.salpini00@gmail.com>
2023-11-12 19:53:43 +0100

    implemented stop and started done

--- api/src/main/java/it/unicam/cs/followme/list/model/commands/basic/Done.java
+++ api/src/main/java/it/unicam/cs/followme/list/model/commands/basic/Done.java
@@ -1,4 +1,17 @@
 package it.unicam.cs.followme.list.model.commands.basic;
 
-public class Done {
+import it.unicam.cs.followme.list.model.commands.Command;
+import it.unicam.cs.followme.list.model.robots.Robot;
+import it.unicam.cs.followme.utilities.RobotCommand;
+
+public class Done implements Command {
+    @Override
+    public RobotCommand getCommandType() {
+        return RobotCommand.DONE;
+    }
+
+    @Override
+    public void run(Robot robot) {
+
+    }
 }
--- api/src/main/java/it/unicam/cs/followme/list/model/commands/basic/Stop.java
+++ api/src/main/java/it/unicam/cs/followme/list/model/commands/basic/Stop.java
@@ -1,4 +1,23 @@
 package it.unicam.cs.followme.list.model.commands.basic;
 
-public class Stop {
+import it.unicam.cs.followme.list.model.Environment;
+import it.unicam.cs.followme.list.model.commands.Command;
+import it.unicam.cs.followme.list.model.robots.Robot;
+import it.unicam.cs.followme.list.model.utils.CartesianCoordinate;
+import it.unicam.cs.followme.utilities.RobotCommand;
+
+public class Stop<R extends Robot> implements Command<R> {
+    private final Environment<R> environment;
+    public Stop(Environment<R> environment) {
+        this.environment = environment;
+    }
+    @Override
+    public RobotCommand getCommandType() {
+        return RobotCommand.STOP;
+    }
+
+    @Override
+    public void run(R robot) {
+        robot.setLastMovementDirection(new CartesianCoordinate(0,0));
+    }
 }
